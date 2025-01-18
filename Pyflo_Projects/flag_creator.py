2bfe93fafd6c4ad91303dc813b614535d7f4ef61
SilverWolf030 <sebgoatie030@gmail.com>
2025-01-17 19:23:07 -0600

    7:30

--- Pyflo_Projects/flag_creator.py
+++ Pyflo_Projects/flag_creator.py
@@ -1,8 +1,19 @@
 def main():
     width = int(input("Flag width:\n"))
     height = int(input("Flag height:\n"))
-    for i in width / 2:
-        print("#" + "-") * height / 2
-        print("-" * width) * height /2
+    half_w = width / 2
+    half_h = height / 2
+    upper_r(half_w,half_h)
+    lower_r(width,half_h)
+    
+def upper_r(hw,hh):
+    while hh > 0:
+        hh -= 1
+        print("#" * int(hw) + "-" * int(hw))
         
+def lower_r(w,hh):
+    while hh > 0:
+        hh -= 1
+        print("-" * int(w))
+                
 main()
