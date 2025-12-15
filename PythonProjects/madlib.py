7b10a8160a24b41b59f804234add71239bdda63f
SilverWolf030 <sebgoatie030@gmail.com>
2025-12-15 15:35:16 -0600

    merged repos

---
+++ PythonProjects/madlib.py
@@ -0,0 +1,20 @@
+# madlib
+# Variables
+person0= input("person:")
+number= input("number:")
+noun1= input("noun:")
+verb0= input("verb:")
+noun2= input("noun:")
+adj0= input("adjuctive:")
+bodyp0= input("bodypart:")
+noun3= input("noun:")
+noun4= input("noun:")
+
+madlib= f"A {person0} went to say good night to there {number} year old {noun1},\
+very well knowing that if they didnt there {noun1} would have trouble {verb0}. It was a \
+nightly routine between them. {person0} entered the dimly lit room where {noun1} waited under \
+the {noun2}. With the first glance the {person0} could tell there was something {adj0} about\
+{noun1} tonight, but couldnt put there {bodyp0} on it. {noun1} looked the same but had a {noun3} \
+that drew from {noun4} to {noun4}."
+
+print(madlib)
